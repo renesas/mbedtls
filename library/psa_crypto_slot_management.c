e8a0eefc4514cca7fcf715c0f45afbb9f31d266c
Michael Thomas <michael.thomas@renesas.com>
2021-03-13 05:19:04 +0000

    Updated export size macro to return value for wrapped keys also fixed missing argument

--- library/psa_crypto_slot_management.c
+++ library/psa_crypto_slot_management.c
@@ -265,7 +265,7 @@ static psa_status_t psa_load_persistent_key_into_slot( psa_key_slot_t *slot )
 #if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
     if (PSA_KEY_TYPE_IS_VENDOR_DEFINED(slot->attr.type))
     {
-        status = psa_import_key_into_slot_vendor( slot, key_data, key_data_length, false);
+        status = psa_import_key_into_slot_vendor( slot, key_data, key_data_length, NULL, false);
             goto exit;
     }
     else

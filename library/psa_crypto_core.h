ee596c7ea5504714ec131562d313f72aa16f722b
Michael Thomas <michael.thomas@renesas.com>
2021-04-01 17:29:57 +0000

    removed commented code

--- library/psa_crypto_core.h
+++ library/psa_crypto_core.h
@@ -160,7 +160,6 @@ static inline void psa_key_slot_set_bits_in_flags( psa_key_slot_t *slot,
 static inline void psa_key_slot_clear_bits( psa_key_slot_t *slot,
                                             uint16_t mask )
 {
-    //slot->attr.flags &= ~mask;
     slot->attr.flags = (psa_key_attributes_flag_t) (slot->attr.flags & (~mask)); 
 }
 

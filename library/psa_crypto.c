819b8b29c176e68b81718b53a72bdb54d0d1cb3e
Lavanya Selvaraju <lavanya.selvaraju@tataelxsi.co.in>
2022-01-05 14:53:38 +0530

    SWFLEX-3898: Changes in psa_crypto file

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -5312,6 +5312,18 @@ psa_status_t psa_generate_key( const psa_key_attributes_t *attributes,
     if( status != PSA_SUCCESS )
         goto exit;
 
+#if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
+    if (PSA_KEY_TYPE_IS_VENDOR_DEFINED(slot->attr.type))
+    {
+        status = psa_generate_key_vendor(slot, attributes->core.bits,
+            attributes->domain_parameters, attributes->domain_parameters_size);
+            goto exit;
+    }
+    else
+	{
+	}
+#endif /* MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C */
+
     /* In the case of a transparent key or an opaque key stored in local
      * storage (thus not in the case of generating a key in a secure element
      * or cryptoprocessor with storage), we have to allocate a buffer to
@@ -5342,15 +5354,7 @@ psa_status_t psa_generate_key( const psa_key_attributes_t *attributes,
         if( status != PSA_SUCCESS )
             goto exit;
     }
-#if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
-    if (PSA_KEY_TYPE_IS_VENDOR_DEFINED(slot->attr.type))
-    {
-        status = psa_generate_key_vendor(slot, attributes->core.bits,
-            attributes->domain_parameters, attributes->domain_parameters_size);
-            goto exit;
-    }
-    else
-#endif /* MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C */
+
     status = psa_driver_wrapper_generate_key( attributes,
         slot->key.data, slot->key.bytes, &slot->key.bytes );
 

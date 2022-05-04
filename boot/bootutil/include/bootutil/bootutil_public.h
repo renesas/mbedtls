54e15f267a9f5be3956c4b219a48f701653daac1
Lavanya Selvaraju <lavanya.selvaraju@tataelxsi.co.in>
2022-05-04 12:19:00 +0530

    SWFLEX-4470: Commented the assert statement

--- boot/bootutil/include/bootutil/bootutil_public.h
+++ boot/bootutil/include/bootutil/bootutil_public.h
@@ -83,8 +83,8 @@ extern "C" {
 
 #ifdef MCUBOOT_BOOT_MAX_ALIGN
 
-_Static_assert(MCUBOOT_BOOT_MAX_ALIGN >= 8 && MCUBOOT_BOOT_MAX_ALIGN <= 32,
-               "Unsupported value for MCUBOOT_BOOT_MAX_ALIGN");
+//_Static_assert(MCUBOOT_BOOT_MAX_ALIGN >= 8 && MCUBOOT_BOOT_MAX_ALIGN <= 32,
+//               "Unsupported value for MCUBOOT_BOOT_MAX_ALIGN");
 
 #define BOOT_MAX_ALIGN          MCUBOOT_BOOT_MAX_ALIGN
 #define BOOT_MAGIC_ALIGN_SIZE   ALIGN_UP(BOOT_MAGIC_SZ, BOOT_MAX_ALIGN)

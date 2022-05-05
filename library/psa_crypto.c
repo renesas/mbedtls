1a6cc415137aa770e157d44df3c7cc98c253ce60
Varun.C <varun.c@tataelxsi.co.in>
2022-05-05 19:51:16 +0530

    Added HW acceleration segment

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -1254,11 +1254,11 @@ psa_status_t psa_get_key_slot_number(
 }
 #endif /* MBEDTLS_PSA_CRYPTO_SE_C */
 
-static psa_status_t psa_export_key_buffer_internal( const uint8_t *key_buffer,
-                                                    size_t key_buffer_size,
-                                                    uint8_t *data,
-                                                    size_t data_size,
-                                                    size_t *data_length )
+psa_status_t psa_export_key_buffer_internal( const uint8_t *key_buffer,
+                                             size_t key_buffer_size,
+                                             uint8_t *data,
+                                             size_t data_size,
+                                             size_t *data_length )
 {
     if( key_buffer_size > data_size )
         return( PSA_ERROR_BUFFER_TOO_SMALL );
@@ -1693,7 +1693,7 @@ static psa_status_t psa_start_key_creation(
  * \return If this function fails, the key slot is an invalid state.
  *         You must call psa_fail_key_creation() to wipe and free the slot.
  */
-static psa_status_t psa_finish_key_creation(
+psa_status_t psa_finish_key_creation(
     psa_key_slot_t *slot,
     psa_se_drv_table_entry_t *driver,
     mbedtls_svc_key_id_t *key)
@@ -1724,6 +1724,13 @@ static psa_status_t psa_finish_key_creation(
         }
         else
 #endif /* MBEDTLS_PSA_CRYPTO_SE_C */
+#if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
+    if (PSA_KEY_TYPE_IS_VENDOR_DEFINED(slot->attr.type))
+    {
+        status = psa_finish_key_creation_vendor( slot );
+    }
+    else
+#endif /* MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C */
         {
             /* Key material is saved in export representation in the slot, so
              * just pass the slot buffer for storage. */
@@ -1926,13 +1933,24 @@ psa_status_t psa_import_key( const psa_key_attributes_t *attributes,
     }
 
     bits = slot->attr.bits;
-    status = psa_driver_wrapper_import_key( attributes,
-                                            data, data_length,
-                                            slot->key.data,
-                                            slot->key.bytes,
-                                            &slot->key.bytes, &bits );
-    if( status != PSA_SUCCESS )
-        goto exit;
+
+#if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
+    if (PSA_KEY_TYPE_IS_VENDOR_DEFINED(slot->attr.type))
+    {
+        status = psa_import_key_into_slot_vendor( attributes, slot, data, data_length, key, true );
+            goto exit;
+    }
+    else
+#endif /* MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C */
+    {
+        status = psa_driver_wrapper_import_key( attributes,
+                                                data, data_length,
+                                                slot->key.data,
+                                                slot->key.bytes,
+                                                &slot->key.bytes, &bits );
+        if( status != PSA_SUCCESS )
+            goto exit;
+    }
 
     if( slot->attr.bits == 0 )
         slot->attr.bits = (psa_key_bits_t) bits;
@@ -5838,7 +5856,15 @@ psa_status_t psa_generate_key( const psa_key_attributes_t *attributes,
         if( status != PSA_SUCCESS )
             goto exit;
     }
-
+#if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
+    if (PSA_KEY_TYPE_IS_VENDOR_DEFINED(slot->attr.type))
+    {
+        status = psa_generate_key_vendor(slot, attributes->core.bits,
+            attributes->domain_parameters, attributes->domain_parameters_size);
+            goto exit;
+    }
+    else
+#endif /* MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C */
     status = psa_driver_wrapper_generate_key( attributes,
         slot->key.data, slot->key.bytes, &slot->key.bytes );
 

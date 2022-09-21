c269ebae1e875a5600613b57a43eb57410b0b95e
Lavanya Selvaraju <lavanya.selvaraju@tataelxsi.co.in>
2022-09-21 11:56:17 +0530

    Temporary change

--- library/platform.c
+++ library/platform.c
@@ -227,7 +227,7 @@ int mbedtls_platform_set_fprintf( int (*fprintf_func)( FILE *, const char *, ...
 #endif /* MBEDTLS_PLATFORM_FPRINTF_ALT */
 
 #if defined(MBEDTLS_PLATFORM_SETBUF_ALT)
-#if !defined(MBEDTLS_PLATFORM_STD_SETBUF)
+//#if !defined(MBEDTLS_PLATFORM_STD_SETBUF)
 /*
  * Make dummy function to prevent NULL pointer dereferences
  */
@@ -238,7 +238,7 @@ static void platform_setbuf_uninit( FILE *stream, char *buf )
 }
 
 #define MBEDTLS_PLATFORM_STD_SETBUF   platform_setbuf_uninit
-#endif /* !MBEDTLS_PLATFORM_STD_SETBUF */
+//#endif /* !MBEDTLS_PLATFORM_STD_SETBUF */
 void (*mbedtls_setbuf)( FILE *stream, char *buf ) = MBEDTLS_PLATFORM_STD_SETBUF;
 
 int mbedtls_platform_set_setbuf( void (*setbuf_func)( FILE *stream, char *buf ) )

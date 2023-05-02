80f5ab432cd2e7651fe004c18b0ec22dc9a82b59
Lavanya Selvaraju <lavanya.selvaraju@tataelxsi.co.in>
2023-05-02 16:32:47 +0530

    Vendor related changes

--- library/psa_crypto_slot_management.c
+++ library/psa_crypto_slot_management.c
@@ -255,7 +255,21 @@ static psa_status_t psa_load_persistent_key_into_slot(psa_key_slot_t *slot)
     }
 #endif /* MBEDTLS_PSA_CRYPTO_SE_C */
 
-    status = psa_copy_key_material_into_slot(slot, key_data, key_data_length);
+#if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
+    psa_key_attributes_t attributes;
+
+    attributes.core = slot->attr;
+
+    if (PSA_KEY_TYPE_IS_VENDOR_DEFINED(slot->attr.type))
+    {
+        status = psa_import_key_into_slot_vendor( &attributes, slot, key_data, key_data_length, NULL, false);
+            goto exit;
+    }
+    else
+#endif /* MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C */
+    {
+        status = psa_copy_key_material_into_slot( slot, key_data, key_data_length );
+    }
 
 exit:
     psa_free_persistent_key_data(key_data, key_data_length);

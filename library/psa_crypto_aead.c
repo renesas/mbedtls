f6ec517c43a5ca89fcf36e077d3b089066a05229
lavanya.selvaraju <lavanya.selvaraju@tataelxsi.co.in>
2023-10-30 00:14:45 -0700

    Fixed build err

--- library/psa_crypto_aead.c
+++ library/psa_crypto_aead.c
@@ -114,10 +114,12 @@ static psa_status_t psa_aead_setup(
             mbedtls_gcm_init(&operation->ctx.gcm);
 
 #if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
+#if defined(MBEDTLS_AES_ALT)
 if ((PSA_KEY_TYPE_IS_VENDOR_DEFINED(attributes->core.type)) && ((key_buffer_size == (SIZE_AES_128BIT_KEYLEN_BYTES_WRAPPED)) || (key_buffer_size == (SIZE_AES_192BIT_KEYLEN_BYTES_WRAPPED)) || (key_buffer_size == (SIZE_AES_256BIT_KEYLEN_BYTES_WRAPPED))))
 {
     operation->ctx.gcm.vendor_flag = 1U;
 }
+#endif
 #endif
 
             status = mbedtls_to_psa_error(

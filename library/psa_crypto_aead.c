55bc38fb0ad8fcf1993a26a10e8bc07dce812394
lavanya.selvaraju <lavanya.selvaraju@tataelxsi.co.in>
2023-10-30 06:21:18 -0700

    Vendor code update

--- library/psa_crypto_aead.c
+++ library/psa_crypto_aead.c
@@ -92,6 +92,14 @@ static psa_status_t psa_aead_setup(
             }
 
             mbedtls_ccm_init(&operation->ctx.ccm);
+#if defined (MBEDTLS_PSA_CRYPTO_ACCEL_DRV_C)
+#if defined(MBEDTLS_CCM_ALT)
+if ((PSA_KEY_TYPE_IS_VENDOR_DEFINED(attributes->core.type)) && ((key_buffer_size == (SIZE_AES_128BIT_KEYLEN_BYTES_WRAPPED)) || (key_buffer_size == (SIZE_AES_192BIT_KEYLEN_BYTES_WRAPPED)) || (key_buffer_size == (SIZE_AES_256BIT_KEYLEN_BYTES_WRAPPED))))
+{
+    operation->ctx.ccm.vendor_flag = 1U;
+}
+#endif
+#endif
             status = mbedtls_to_psa_error(
                 mbedtls_ccm_setkey(&operation->ctx.ccm, cipher_id,
                                    key_buffer, (unsigned int) key_bits));

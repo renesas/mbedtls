201abad13d88ebf8a97e1ae3ec683c6061ad65bd
Hien Huynh <hien.huynh.rj@renesas.com>
2024-01-25 00:12:25 +0700

    FSPRA-2329 synchronize with tinycrypt submodule

--- ext/tinycrypt/lib/source/ccm_mode.c
+++ ext/tinycrypt/lib/source/ccm_mode.c
@@ -33,10 +33,9 @@
 #include <tinycrypt/ccm_mode.h>
 #include <tinycrypt/constants.h>
 #include <tinycrypt/utils.h>
-#include "bsp_api.h"
 #include <stdio.h>
 
-#if (BSP_FEATURE_CRYPTO_AES_IP_VERSION != 2)
+#if ((BSP_FEATURE_CRYPTO_AES_IP_VERSION != 2) || ((BSP_FEATURE_CRYPTO_AES_IP_VERSION == 2) && (RM_TINYCRYPT_PORT_CCM_SW_USING == 1)))
 int tc_ccm_config(TCCcmMode_t c, TCAesKeySched_t sched, uint8_t *nonce,
 		  unsigned int nlen, unsigned int mlen)
 {

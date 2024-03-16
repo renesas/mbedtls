ec84f6d439eceb750c4ba31cee2f5d38c34d9960
lavanya.selvaraju.cj <lavanya.selvaraju.cj@bp.renesas.com>
2024-03-17 00:22:58 +0530

    Updated hdr and src files

--- boot/bootutil/include/bootutil/crypto/common.h
+++ boot/bootutil/include/bootutil/crypto/common.h
@@ -7,6 +7,13 @@
 #ifndef __BOOTUTIL_CRYPTO_COMMON_H__
 #define __BOOTUTIL_CRYPTO_COMMON_H__
 
+/** Allow library to access its structs' private members.
+ *
+ * Although structs defined in header files are publicly available,
+ * their members are private and should not be accessed by the user.
+ */
+#define MBEDTLS_ALLOW_PRIVATE_ACCESS
+
 /* The check below can be performed even for those cases
  * where MCUBOOT_USE_MBED_TLS has not been defined
  */

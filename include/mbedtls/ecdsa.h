14a172496564e20294532f23b48e4e80a36a9d59
Hien Huynh <hien.huynh.rj@renesas.com>
2024-04-09 00:05:00 +0700

    FSPRA-2555 update mbedtls_eddsa_sign

--- include/mbedtls/ecdsa.h
+++ include/mbedtls/ecdsa.h
@@ -175,7 +175,7 @@ int mbedtls_ecdsa_sign(mbedtls_ecp_group *grp, mbedtls_mpi *r, mbedtls_mpi *s,
                        int (*f_rng)(void *, unsigned char *, size_t), void *p_rng);
 
 int mbedtls_eddsa_sign(mbedtls_ecp_group *grp, mbedtls_mpi *r, mbedtls_mpi *s,
-                       const mbedtls_mpi *d, const unsigned char *buf, size_t blen,
+                       const mbedtls_mpi *d, const mbedtls_mpi *Q, const unsigned char *buf, size_t blen,
                        int (*f_rng)(void *, unsigned char *, size_t), void *p_rng);
 
 #if defined(MBEDTLS_ECDSA_DETERMINISTIC)
--- library/psa_crypto_ecp.c
+++ library/psa_crypto_ecp.c
@@ -560,7 +560,7 @@ psa_status_t mbedtls_psa_eddsa_sign_hash(
     }
 
     (void) alg;
-    MBEDTLS_MPI_CHK(mbedtls_eddsa_sign(&ecp->grp, &r, &s, &ecp->d,
+    MBEDTLS_MPI_CHK(mbedtls_eddsa_sign(&ecp->grp, &r, &s, &ecp->d, &ecp->Q,
                                            hash, hash_length,
                                            mbedtls_psa_get_random,
                                            MBEDTLS_PSA_RANDOM_STATE));

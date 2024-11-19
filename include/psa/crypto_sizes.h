8914ffe385aedc51ee5564b4379cffd132725de9
lavanya.selvaraju.cj <lavanya.selvaraju.cj@bp.renesas.com>
2024-11-19 20:04:50 +0530

    Revert "Test change"
    This reverts commit 2cb2836dd259f137c3ddce1808673922e8b4257f.

--- include/psa/crypto_sizes.h
+++ include/psa/crypto_sizes.h
@@ -37,8 +37,8 @@
  */
 #include "psa/build_info.h"
 
-#define PSA_BITS_TO_BYTES(bits) (((bits) + 7u) / 8u)
-#define PSA_BYTES_TO_BITS(bytes) ((bytes) * 8u)
+#define PSA_BITS_TO_BYTES(bits) (((bits) + 7U) / 8U)
+#define PSA_BYTES_TO_BITS(bytes) ((bytes) * 8U)
 #define PSA_MAX_OF_THREE(a, b, c) ((a) <= (b) ? (b) <= (c) ? \
                                    (c) : (b) : (a) <= (c) ? (c) : (a))
 
